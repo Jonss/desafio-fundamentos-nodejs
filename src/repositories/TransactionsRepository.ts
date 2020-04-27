561c83c16427e66691a7ac6a2d1cd301c3f50463
Joao Santana <joaosantana.ti@gmail.com>
2020-04-27 09:52:47 -0300

    Corrigindo teste

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -26,17 +26,24 @@ class TransactionsRepository {
   public getBalance(): Balance {
     const income = this.sumValues('income');
     const outcome = this.sumValues('outcome');
-
     return { income, outcome, total: income - outcome };
   }
 
   public create({ title, value, type }: TransactionDTO): Transaction {
+    if (type === 'outcome' && value > this.getBalance().total) {
+      throw Error('Outcome not permitted');
+    }
+
     const tx = new Transaction({ title, value, type });
     this.transactions.push(tx);
     return tx;
   }
 
   private sumValues(type: string): number {
+    if (this.transactions.filter(tx => tx.type === type).length === 0) {
+      return 0;
+    }
+
     return this.transactions
       .filter(tx => tx.type === type)
       .map(tx => tx.value)
